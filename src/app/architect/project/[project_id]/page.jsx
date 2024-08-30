e3dc5a84e3867ceb14a3e04bf4a162425c58649f
Gabriel Matte Elias <gabrielmatteelias@gmail.com>
2024-08-30 17:02:36 -0300

    layout project

--- src/app/architect/project/[project_id]/page.jsx
+++ src/app/architect/project/[project_id]/page.jsx
@@ -1,12 +1,18 @@
 import { Container } from '@/components/Container';
 import styles from './page.module.css'
-import { ProjectInfo } from './components/info';
 import AvatarImageGroup from '@/components/AvatarGroup';
 
 import gabriel from '@/assets/gabriel_arquiteto.png'
 import fernando from '@/assets/fernando_arquiteto.png'
 import bernardo from '@/assets/bernardo_arquiteto.png'
 import ProfileAvatar from '@/components/Avatar';
+import Image from 'next/image';
+
+import foto1 from '@/assets/foto1.png'
+import foto4 from '@/assets/foto4.png'
+import foto5 from '@/assets/foto5.png'
+import foto6 from '@/assets/foto6.png'
+import foto7 from '@/assets/foto7.png'
 
 const contributors = [
     {
@@ -82,7 +88,57 @@ export default function ({ params }) {
                         </ul>
                     </div>
                 </section>
-                <ProjectInfo />
+
+                <section>
+                    <div className={styles.gallery}>
+                        <div className={styles.main_image}>
+                            <Image
+                                alt={'Imagem'}
+                                quality={100}
+                                priority
+                                src={foto7}
+                            />
+                        </div>
+
+                        <div className={styles.secondary_images}>
+                            <Image
+                                alt={'Imagem'}
+                                quality={100}
+                                priority
+                                src={foto6}
+                            />
+                            <Image
+                                alt={'Imagem'}
+                                quality={100}
+                                priority
+                                src={foto5}
+                            />
+                            <Image
+                                alt={'Imagem'}
+                                quality={100}
+                                priority
+                                src={foto4}
+                            />
+                            <Image
+                                alt={'Imagem'}
+                                quality={100}
+                                priority
+                                src={foto1}
+                            />
+                        </div>
+                    </div>
+
+                    <section className={styles.description}>
+                        <p>
+                            A reforma do Grand Palais em Paris visa preservar e modernizar este ícone da arquitetura Beaux-Arts,
+                            inaugurado em 1900. O projeto, liderado por especialistas, restaurará a cúpula de vidro e ferro, melhorará a
+                            eficiência energética e atualizará a infraestrutura para atender às normas de segurança e acessibilidade.
+                            Os espaços internos serão reorganizados para maior flexibilidade, com novos acessos criados. A intervenção
+                            equilibra a preservação do legado histórico com as necessidades contemporâneas.
+                        </p>
+                    </section>
+                </section>
+
                 <section className={styles.architect_content}>
                     <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
                         <ProfileAvatar
@@ -101,6 +157,7 @@ export default function ({ params }) {
                     <div className={styles.contributors_profile}></div>
                     <AvatarImageGroup avatars={contributors} />
                 </section>
+
             </Container>
         </main>
     )
